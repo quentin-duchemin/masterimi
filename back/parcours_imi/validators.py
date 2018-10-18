d9d8212a42541a7be97512888a4ccfbf35f36091
Louis Trezzini <louis.trezzini@ponts.org>
2018-10-18 22:49:43 +0200

    More info for admins

--- back/parcours_imi/validators.py
+++ back/parcours_imi/validators.py
@@ -4,8 +4,6 @@ from typing import Iterable
 
 from intervaltree import IntervalTree
 
-from parcours_imi.models import AttributeConstraint
-
 
 class ConstraintType(Enum):
     VALID = 'valid'
@@ -32,7 +30,7 @@ def build_validation_entry(validation_message: str, constraint_type: ConstraintT
 
 class AttributeConstraintsValidator:
 
-    def __init__(self, constraints: Iterable[AttributeConstraint], attributes_getter):
+    def __init__(self, constraints: Iterable['AttributeConstraint'], attributes_getter):
         self.constraints = constraints
         self.attributes_getter = attributes_getter
 
@@ -107,3 +105,31 @@ class TimeCollisionValidator:
     def time_to_seconds(self, time: str):
         hours, minutes = time.split(':')
         return int(hours) * 60 + int(minutes)
+
+
+def get_parcours_courses_rules_validation_data(parcours, main_courses, option_courses):
+    attribute_constraints_validator = AttributeConstraintsValidator(
+        constraints=parcours.master.attribute_constraints.all(),
+        attributes_getter=lambda course: course.attributes,
+    )
+    attribute_constraints_validation_data = attribute_constraints_validator.validate(
+        main_courses + option_courses
+    )
+
+    time_collision_validation_data = TimeCollisionValidator().validate(
+        main_courses + option_courses
+    )
+
+    if parcours.option == '3A-M2-ECTS':
+        from parcours_imi.models import AttributeConstraint
+        option_validator = AttributeConstraintsValidator(
+            constraints=[
+                AttributeConstraint.objects.get(pk='8ce0b5ea-7e1d-4d7a-b5e6-77c83ed0d4d9')
+            ],
+            attributes_getter=lambda course: course.attributes,
+        )
+        option_validation_data = option_validator.validate(option_courses)
+
+        return attribute_constraints_validation_data + option_validation_data + time_collision_validation_data
+
+    return attribute_constraints_validation_data + time_collision_validation_data
