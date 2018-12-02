4449c8a0121615361620f0f84bead491b9838bac
Louis Trezzini <louis.trezzini@ponts.org>
2018-12-02 18:49:15 +0100

    fix settings

--- back/master_imi/settings.py
+++ back/master_imi/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='-fuknloe3!gj$p=fia@y=-byvr4=v0tr905(0gvk)t_mmklte1')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
