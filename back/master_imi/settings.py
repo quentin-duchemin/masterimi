640766c7d1f989af3702ec5ac942e3fa4ff492c1
Louis Trezzini <louis@feedly.com>
2021-09-09 14:28:02 +0200

    Upgrade djangorestframework-camel-case

--- back/master_imi/settings.py
+++ back/master_imi/settings.py
@@ -120,8 +120,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ],
 }
